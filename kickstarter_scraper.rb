087eadff10381d78b8616140dcea45343709729d
Vimal Soni <vimal.h.soni@gmail.com>
2019-04-05 00:48:19 +0000

    Done.

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,11 @@
 # require libraries/modules here
+require 'pry'
+require 'nokogiri'
 
 def create_project_hash
+  html = File.read('fixtures/kickstarter.html')
+
+  kickstarter = Nokogiri::HTML(html)
   # write your code here
+    binding.pry
 end
